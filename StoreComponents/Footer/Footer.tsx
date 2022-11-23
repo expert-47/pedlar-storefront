1f01a8fd34ca5cc8aaadf8797a96663d36b56f9d
numan abid <numan@gsoftconsulting.com>
2022-11-23 16:20:01 +0500

    animation+ footer

--- StoreComponents/Footer/Footer.tsx
+++ StoreComponents/Footer/Footer.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { Box, Grid, Typography} from "@mui/material";
+import Link from '@mui/material/Link';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { styles } from "./style";
 
 const Footer = () => {
   return (
-    <div>
-      <h2> This is Footer</h2>
-    </div>
+    
+    <Box sx={styles.staticFooter}>
+      <Grid container item xs={12} sm={12} md={12} lg={12} sx={styles.footerRow}>
+        <Grid item xs={12} sm={12} md={12} lg={5}>
+            <Box>
+              <img src="/footer-logo.svg" alt="" />
+            </Box>
+            <Typography sx={styles.footerText}>We’re always here to help.</Typography>
+            <Typography sx={styles.footerText} style={{margin: '0 auto 0 0',  maxWidth: '435px'}}>Contact us at hello@pedlar.store and our team will be in touch.</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={3}>
+            <Link href="#" sx={styles.footerLink}>FAQs</Link>
+            <Link href="#" sx={styles.footerLink}>Shipping & Returns</Link>
+            <Link href="#" sx={styles.footerLink}>Creator Terms & Conditions</Link>
+            <Link href="#" sx={styles.footerLink}>Brands Terms & Conditions</Link>
+            <Link href="#" sx={styles.footerLink}>Privacy Policy</Link>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={3}>
+          <Box style={{display:"flex", alignItems:'center'}}>
+            <InstagramIcon style={{color:"#fff",fontSize:"32px"}} />
+            <Link href="#" sx={styles.footersocial}>Instagram</Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
--- StoreComponents/Footer/style.ts
+++ StoreComponents/Footer/style.ts
@@ -1,3 +1,33 @@
 export const styles ={
-    
+    footerLink: {
+        fontFamily: 'Inter',
+        display:'block',
+        textDecoration:'none',
+        fontSize:'22px',
+        paddingBottom:"16px",
+        fontWeight:'800'
+    },
+    footerText : {
+        fontFamily: 'Inter',
+        paddingBottom:"18px",
+        fontSize:'22px',
+        fontWeight:'500',
+        color:'#fff',
+    },
+    footersocial : {
+        fontFamily: 'Inter',
+        color:'#fff',
+        fontSize:'22px',
+        fontWeight:'800',
+        textDecoration:"none",
+        paddingLeft:"16px"
+    },
+    staticFooter : {
+        background: '#1C1B1F',
+        borderRadius:' 16px 16px 0px 0px',
+        padding:' 80px 0'
+    },
+    footerRow : {
+        justifyContent:'space-between',
+    }
 };

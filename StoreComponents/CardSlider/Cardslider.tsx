12a5198f69b488a2ae14e1a8d3db232eff2c8e49
raza mughal <raza@gsoftconsulting.com>
2022-11-24 15:46:50 +0500

    store

--- StoreComponents/CardSlider/Cardslider.tsx
+++ StoreComponents/CardSlider/Cardslider.tsx
@@ -1,76 +1,70 @@
-import * as React from 'react';
+import * as React from "react";
 import { Box, Typography } from "@mui/material";
 import { styles } from "./style";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import dynamic from "next/dynamic";
+import { CustomContainer } from "StoreComponents/Layout";
 
 const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
   ssr: false,
 });
-var $ = require("jquery");
+
+const demo = require("jQuery");
 if (typeof window !== "undefined") {
-  // Client-side-only code
-  window.$ = window.jQuery = require("jquery");
+  window.demo = window.jQuery = require("jquery");
 }
 
 const Cardslider = () => {
-    const Responsive = {
-        0: {
-          items : 1,
-          stagePadding: 80,
-        },
-        600: {
-            items : 3,
-            stagePadding: 80,
-          },
-       800: {
-          items : 3,
-          stagePadding: 80,
-        },
-        1000: {
-          items : 4,
-        },
-      };
+  const Responsive = {
+    0: {
+      items: 1,
+      stagePadding: 80,
+    },
+    600: {
+      items: 3,
+      stagePadding: 80,
+    },
+    800: {
+      items: 3,
+      stagePadding: 80,
+    },
+    1000: {
+      items: 4,
+    },
+  };
   return (
-   <Box style={{paddingTop:'100px'}}>
-        <OwlCarousel
-            responsive={Responsive}
-            margin={15}
-            loop
-            stagePadding={100}
-            autoplay={true}
-            autoplayTimeout={2000}
-        >
-        <Box className="item" style={{padding:"10px 0"}}>
+    <CustomContainer>
+      <Box style={{ paddingTop: "100px" }}>
+        <OwlCarousel responsive={Responsive} margin={15} loop stagePadding={100} autoplay={true} autoplayTimeout={2000}>
+          <Box className="item" style={{ padding: "10px 0" }}>
             <Box sx={styles.cardBox}>
-                <img src='/demoo.png' />
-                <Typography sx={styles.cardText}>Mora Ruby</Typography>
+              <img src="/demoo.png" />
+              <Typography sx={styles.cardText}>Mora Ruby</Typography>
             </Box>
-        </Box>
-        <Box className="item" style={{padding:"10px 0"}}>
+          </Box>
+          <Box className="item" style={{ padding: "10px 0" }}>
             <Box sx={styles.cardBox}>
-               <img src='/debbi.png' />
-                <Typography sx={styles.cardText}>Debbi Heart</Typography>
+              <img src="/debbi.png" />
+              <Typography sx={styles.cardText}>Debbi Heart</Typography>
             </Box>
-        </Box>
-        <Box className="item" style={{padding:"10px 0"}}>
+          </Box>
+          <Box className="item" style={{ padding: "10px 0" }}>
             <Box sx={styles.cardBox}>
-               <img src='/rename.png' />
-                <Typography sx={styles.cardText}>Shadow</Typography>
+              <img src="/rename.png" />
+              <Typography sx={styles.cardText}>Shadow</Typography>
             </Box>
-        </Box>
-        <Box className="item" style={{padding:"10px 0"}}>
+          </Box>
+          <Box className="item" style={{ padding: "10px 0" }}>
             <Box sx={styles.cardBox}>
-               <img src='/stephen.png' />
-                <Typography sx={styles.cardText}>Stephen Dan Smith</Typography>
+              <img src="/stephen.png" />
+              <Typography sx={styles.cardText}>Stephen Dan Smith</Typography>
             </Box>
-        </Box>
-      
-       
-      </OwlCarousel>
-   </Box>
-  )
-}
+          </Box>
+        </OwlCarousel>
+      </Box>
+    </CustomContainer>
+  );
+};
 
-export default Cardslider
+export default Cardslider;

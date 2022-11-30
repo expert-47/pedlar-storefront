150b6e81d082b3a011075f8c85d8205f0c46b8fb
raza mughal <raza@gsoftconsulting.com>
2022-11-30 10:32:49 +0500

    carousel

--- StoreComponents/landingpage/picture-carousel/picturecarousel.tsx
+++ StoreComponents/landingpage/picture-carousel/picturecarousel.tsx
@@ -1,38 +1,55 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
 import { Box, Typography } from "@mui/material";
 import { styles } from "./style";
-
+import "swiper/css";
 
 const Picturecarousel = () => {
   return (
     <Box style={{ paddingTop: "100px" }}>
-      <Carousel autoPlay interval={2000} transitionTime={2000} className="carousal">
-        <Box className="item" style={{ padding: "10px 0" }}>
-          <Box sx={styles.cardBox}>
-            <img src="/demoo.png" />
-            <Typography sx={styles.cardText}>Mora Ruby</Typography>
+      <Swiper
+        centeredSlides={true}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide style={{ margin: "10px" }}>
+          <Box className="item" style={{ padding: "10px 0" }}>
+            <Box sx={styles.cardBox}>
+              <img src="/demoo.png" />
+              <Typography sx={styles.cardText}>Mora Ruby</Typography>
+            </Box>
           </Box>
-        </Box>
-        <Box className="item" style={{ padding: "10px 0" }}>
-          <Box sx={styles.cardBox}>
-            <img src="/debbi.png" />
-            <Typography sx={styles.cardText}>Debbi Heart</Typography>
+        </SwiperSlide>
+        <SwiperSlide style={{ margin: "10px" }}>
+          <Box className="item" style={{ padding: "10px 0" }}>
+            <Box sx={styles.cardBox}>
+              <img src="/debbi.png" />
+              <Typography sx={styles.cardText}>Debbi Heart</Typography>
+            </Box>
           </Box>
-        </Box>
-        <Box className="item" style={{ padding: "10px 0" }}>
-          <Box sx={styles.cardBox}>
-            <img src="/rename.png" />
-            <Typography sx={styles.cardText}>Shadow</Typography>
+        </SwiperSlide>
+        <SwiperSlide style={{ margin: "10px" }}>
+          <Box className="item" style={{ padding: "10px 0" }}>
+            <Box sx={styles.cardBox}>
+              <img src="/rename.png" />
+              <Typography sx={styles.cardText}>Shadow</Typography>
+            </Box>
           </Box>
-        </Box>
-        <Box className="item" style={{ padding: "10px 0" }}>
-          <Box sx={styles.cardBox}>
-            <img src="/stephen.png" />
-            <Typography sx={styles.cardText}>Stephen Dan Smith</Typography>
+        </SwiperSlide>
+        <SwiperSlide style={{ margin: "10px" }}>
+          <Box className="item" style={{ padding: "10px 0" }}>
+            <Box sx={styles.cardBox}>
+              <img src="/stephen.png" />
+              <Typography sx={styles.cardText}>Stephen Dan Smith</Typography>
+            </Box>
           </Box>
-        </Box>
-      </Carousel>
+        </SwiperSlide>
+      </Swiper>
     </Box>
   );
 };

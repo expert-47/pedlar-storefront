009c2bac9e37433238f6a1bff74a31b26d1053c1
raza mughal <raza@gsoftconsulting.com>
2022-12-08 14:46:22 +0500

    Changed Images tags

--- landing-components/landing/how-its-work/how-its-work.tsx
+++ landing-components/landing/how-its-work/how-its-work.tsx
@@ -1,13 +1,16 @@
 import React, { useRef } from "react";
 import { styles } from "./style";
-import { Box, Grid, Typography, List, useTheme } from "@mui/material";
+import { Box, Grid, Typography, List, useTheme, useMediaQuery } from "@mui/material";
 import { CustomContainer } from "../../landinglayout";
 import { Scrollspy } from "@makotot/ghostui";
 import Content from "./content.json";
 import Images from "./images.json";
+import Responsivehowitworks from "./responsive-how-it-works";
 
 const Howitswork = () => {
   const theme = useTheme();
+  const isResponsive = useMediaQuery("(max-width:1023px)");
+
   const sectionRefs = [
     useRef<HTMLDivElement>(null),
     useRef<HTMLDivElement>(null),
@@ -26,98 +29,116 @@ const Howitswork = () => {
         }}
       >
         <Box sx={styles.MainBox}>
-          <Grid sx={styles.Heading}>
-            <Typography fontWeight={700} fontSize={{ xs: "40px", sm: "40px", md: "54px", lg: "54px" }}>
-              How it works
-            </Typography>
-          </Grid>
-
-          <Scrollspy sectionRefs={sectionRefs} offset={-150}>
-            {({ currentElementIndexInViewport }) => (
-              <Grid
-                style={{
-                  display: "flex",
-                  zIndex: 4,
-                  position: "relative",
-                }}
-                container
-                item
-                xs={12}
-                sm={12}
-                md={12}
-                lg={12}
-              >
-                <Grid item xs={8} sm={8} md={8} lg={8} data-cy="section-wrapper">
-                  <List
-                    sx={{
-                      position: "sticky",
-                      top: "125px",
-                      "& .css-16eg70r-MuiList-root": {
-                        position: "sticky",
-                        top: "0",
-                        width: "100%",
-                        mergin: 0,
-                      },
-                    }}
-                    style={{
-                      position: "sticky",
-                      top: "0",
-                      width: "100%",
-                    }}
-                  >
-                    {Content.map((content, key) => (
-                      <Grid
-                        id={`section-${key}`}
-                        key={key}
-                        data-cy={`section-item`}
-                        ref={sectionRefs[key]}
-                        container
-                        item
-                        md={12}
-                        sx={styles.Texts}
-                      >
-                        <Grid
-                          item
-                          xs={0.7}
-                          sm={0.7}
-                          md={0.7}
-                          lg={0.7}
-                          sx={[
-                            styles.circlecount,
-                            {
-                              backgroundColor: currentElementIndexInViewport == key ? "white" : "tranparent",
-                              color: currentElementIndexInViewport == key ? "#1c1b1f" : "white",
-                            },
-                          ]}
-                        >
-                          <Typography fontSize={{ sx: "30px", sm: "30px", md: "30px", lg: "30px" }} fontWeight={600}>
-                            {content.id}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={9} sm={9} md={9} lg={10} style={{ padding: "0px 0 0 28px" }}>
-                          <Typography fontSize={{ sx: "30px", sm: "30px", md: "40px", lg: "40px" }} fontWeight={600}>
-                            {content.text}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    ))}
-                  </List>
-                </Grid>
-                <Grid item xs={4} sm={4} md={4} lg={4} data-cy="nav-wrapper">
-                  {Images.map((image, i) => {
-                    console.log(currentElementIndexInViewport);
-                    if (currentElementIndexInViewport == i) {
-                      return (
-                        <Grid md={6} data-cy={`nav-item`}>
-                          <img src={image.img} alt="home-sec5-img1" style={{ borderRadius: "16px" }} />
-                        </Grid>
-                      );
-                    }
-                  })}
-                </Grid>
+          {isResponsive ? (
+            <>
+              <Responsivehowitworks />
+            </>
+          ) : (
+            <>
+              <Grid sx={styles.Heading}>
+                <Typography fontWeight={700} fontSize={{ xs: "40px", sm: "40px", md: "54px", lg: "54px" }}>
+                  How it works
+                </Typography>
               </Grid>
-            )}
-          </Scrollspy>
+              <Scrollspy sectionRefs={sectionRefs} offset={-150}>
+                {({ currentElementIndexInViewport }) => (
+                  <Grid
+                    style={{
+                      display: "flex",
+                      zIndex: 4,
+                      position: "relative",
+                    }}
+                    container
+                    item
+                    xs={12}
+                    sm={12}
+                    md={12}
+                    lg={12}
+                  >
+                    <Grid item xs={12} sm={12} md={8} lg={8} data-cy="section-wrapper">
+                      <List
+                        sx={{
+                          position: "sticky",
+                          top: "125px",
+                          "& .css-16eg70r-MuiList-root": {
+                            position: "sticky",
+                            top: "0",
+                            width: "100%",
+                            mergin: 0,
+                          },
+                        }}
+                        style={{
+                          position: "sticky",
+                          top: "0",
+                          width: "100%",
+                        }}
+                      >
+                        {Content.map((content, key) => (
+                          <Grid
+                            id={`section-${key}`}
+                            key={key}
+                            data-cy={`section-item`}
+                            ref={sectionRefs[key]}
+                            container
+                            item
+                            xs={12}
+                            sm={12}
+                            md={12}
+                            lg={12}
+                            sx={styles.Texts}
+                          >
+                            <Grid
+                              item
+                              xs={0.7}
+                              sm={0.7}
+                              md={0.7}
+                              lg={0.7}
+                              sx={[
+                                styles.circlecount,
+                                {
+                                  backgroundColor: currentElementIndexInViewport == key ? "white" : "tranparent",
+                                  color: currentElementIndexInViewport == key ? "#1c1b1f" : "white",
+                                },
+                              ]}
+                            >
+                              <Typography
+                                fontSize={{ sx: "30px", sm: "30px", md: "30px", lg: "30px" }}
+                                fontWeight={600}
+                              >
+                                {content.id}
+                              </Typography>
+                            </Grid>
+                            <Grid item xs={9} sm={9} md={9} lg={10} style={{ padding: "0px 0 0 28px" }}>
+                              <Typography
+                                fontSize={{ sx: "30px", sm: "30px", md: "40px", lg: "40px" }}
+                                fontWeight={600}
+                              >
+                                {content.text}
+                              </Typography>
+                            </Grid>
+                          </Grid>
+                        ))}
+                      </List>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4} data-cy="nav-wrapper">
+                      {Images.map((image, i) => {
+                        console.log(currentElementIndexInViewport);
+                        if (currentElementIndexInViewport == i) {
+                          return (
+                            <img
+                              src={image.img}
+                              alt="home-sec5-img1"
+                              style={{ borderRadius: "16px", height: "600px", width: "300px" }}
+                            />
+                          );
+                        }
+                      })}
+                    </Grid>
+                  </Grid>
+                )}
+              </Scrollspy>
+            </>
+          )}
         </Box>
       </Box>
     </CustomContainer>

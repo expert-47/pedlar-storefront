6957d944e9ace733ef25453abbeae7b2e3cb8062
numan abid <numan@gsoftconsulting.com>
2022-12-12 18:57:06 +0500

    faq scrollspy content

--- pages/for-brands.tsx
+++ pages/for-brands.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Faq from "../landing-components/creator-page/faq/Faq";
+import Faq from "../landing-components/brand-page/faq/Faq";
 import Banner from "../landing-components/brand-page/mainbanner/Banner";
 import Layout from "../landing-components/landinglayout";
 import Pointofpurchase from "../landing-components/brand-page/pointofpurchase/Pointofpurchase";
@@ -25,13 +25,13 @@ const brands = () => {
         <Box style={{ position: "absolute", left: "0", width: "auto", zIndex: "0" }} top={{ xs: "0px", lg: "0px" }}>
           <Image src={orangetop} alt="Orange meshes" />
         </Box>
-        <Box
+        {/* <Box
           style={{ position: "absolute", right: "0", width: "auto", zIndex: "0" }}
           top={{ xs: "4083px", sm: "1806px", md: "1134px", lg: "1431px" }}
           bottom={{ xs: "0px", lg: "0px" }}
         >
           <Image src={orangebottom} alt="orang brand meshes" />
-        </Box>
+        </Box> */}
       </Box>
       <Banner />
       <Pointofpurchase />

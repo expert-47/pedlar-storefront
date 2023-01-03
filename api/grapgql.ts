1d0234c621f1647154226992089a6de45b6d0f93
qasim nouman <qasim@gsoftconsulting.com>
2023-01-03 18:50:43 +0500

    api integ on product details

---
+++ api/grapgql.ts
@@ -0,0 +1,57 @@
+import { apiCall } from "./graphqlApi";
+
+export const getProductDetails = async () => {
+  const requestBody = {
+    query: `query GetProduct($productId: ID!, $selectedOptionInput: [SelectedOptionInput!]!) {
+        product(id: $productId) {
+            id
+            title
+            productType
+            vendor
+            description
+            totalInventory
+            priceRange {
+                maxVariantPrice {
+                    amount
+                    currencyCode
+                }
+                minVariantPrice {
+                    amount
+                    currencyCode
+                }
+            }
+            options {
+                id
+                name
+                values
+            }
+            variantBySelectedOptions(selectedOptions: $selectedOptionInput) {
+                id
+                title
+            }
+            featuredImage {
+                url
+            }
+            createdAt
+            publishedAt
+        }
+    }`,
+    variables: {
+      productId: "gid://shopify/Product/7609492340963",
+      selectedOptionInput: [
+        {
+          name: "Size",
+          value: "4 US / 4 AU / 35 EU",
+        },
+        {
+          name: "Color",
+          value: "Brown",
+        },
+      ],
+    },
+  };
+
+  const collectionData = await apiCall(requestBody);
+
+  return collectionData;
+};

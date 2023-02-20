00075cc6d11acf422388b3dd906ad06f648a3d17
Raza Mughal <raza@gsoftconsulting.com>
2023-02-20 17:58:25 +0500

    feature/storfront-ui-issues

--- components/product/components/options.tsx
+++ components/product/components/options.tsx
@@ -19,7 +19,7 @@ function Options(props: any) {
   return (
     <Grid container item xs={12} sm={12} md={12} lg={12} gap={22} sx={styles.container}>
       {newAdditionData?.options[0]?.values?.length > 1 && (
-        <Grid item xs={12} sm={12} md={5.6} lg={5.6} sx={{ marginRight: { lg: "2.5rem !important" } }}>
+        <Grid item xs={12} sm={12} md={5.6} lg={6.5}>
           <Typography sx={styles.typography}>{newAdditionData?.options[0]?.name}</Typography>
           <FormControl sx={{ width: "100%" }}>
             <Select

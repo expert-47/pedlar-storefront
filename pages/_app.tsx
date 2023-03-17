f1913ef8990cda4813e71712d6df149b00f15f43
ahtisham@gsoftconsulting.com <ahtisham@gsoftconsulting.com>
2023-03-17 10:56:29 +0500

    fix: gmt tags according to next js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,6 +36,19 @@ function MyApp({ Component, pageProps }: any) {
 
   return (
     <Fragment>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-WGHPTBF');
+          `,
+        }}
+      />
       <DefaultSeo {...SEO} />
       <Provider store={store}>
         <ApolloProvider client={client}>

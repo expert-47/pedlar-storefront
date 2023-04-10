b33abce194eebdc0fc8438fc7534d7ca55d01cf3
ahtisham@gsoftconsulting.com <ahtisham@gsoftconsulting.com>
2023-04-10 15:41:56 +0500

    fix: gtm tags formating

--- utils/gtm.ts
+++ utils/gtm.ts
@@ -17,50 +17,48 @@ export const beginCheckout = (item: any, type = "checkout button") => {
   if (!Array.isArray(item)) {
     return;
   }
+  let items = item.map((data, index) => {
+    return {
+      currency: data?.merchandise?.price?.currencyCode || "", // Currency
+      item_name: data?.merchandise?.product?.title || "", // Name or ID is required.
+      item_id: data?.merchandise?.product?.id
+        ? data?.merchandise?.product?.id.split("/")[data?.merchandise?.product?.id.split("/").length - 1]
+        : "", //ID of the item.
+      price: parseFloat(data?.merchandise?.price?.amount || ""), //total price of the item.
+      item_brand: data?.merchandise?.product?.vendor || "", // brand of the item.(this is the example value)
+      ...(data?.merchandise?.product?.productType &&
+        data?.merchandise?.product?.productType != "" && {
+          item_category: data?.merchandise?.product?.productType || "",
+        }), //The category to which the product belongs to.
+      ...(data?.merchandise?.selectedOptions[0]?.value &&
+        data?.merchandise?.selectedOptions[0]?.value != "" &&
+        data?.merchandise?.selectedOptions.length != 1 && {
+          item_category2: data?.merchandise?.selectedOptions[0]?.value || "",
+        }), //size of the product.
+      ...(data?.merchandise?.selectedOptions[1]?.value &&
+        data?.merchandise?.selectedOptions[1]?.value != "" && {
+          item_variant: data?.merchandise?.selectedOptions[1]?.value || "",
+        }), // color of the product.
+      ...(data?.merchandise?.selectedOptions[0]?.value &&
+        data?.merchandise?.selectedOptions[0]?.value != "" &&
+        data?.merchandise?.selectedOptions.length == 1 && {
+          item_variant: data?.merchandise?.selectedOptions[0]?.value || "",
+        }), // color of the product.
+      item_category3: type,
+      //  item_list_name: "Category Page",//e.g. Filter results, Popular Picks For You ,Recently Viewed, Best sellers, Search Results, Personal Boutique etc.
+      //  item_list_id: "H3123", //ID of the list in which the item was presented to the user.
+      index: index + 1, // position of the item
+      quantity: data?.quantity, //quantity of the item
 
-  item.map((data: any, index) => {
-    return gtmEvents({
-      event: "begin_checkout",
-      ecommerce: {
-        items: [
-          {
-            currency: data?.merchandise?.price?.currencyCode || "", // Currency
-            item_name: data?.merchandise?.product?.title || "", // Name or ID is required.
-            item_id: data?.merchandise?.product?.id
-              ? data?.merchandise?.product?.id.split("/")[data?.merchandise?.product?.id.split("/").length - 1]
-              : "", //ID of the item.
-            price: parseFloat(data?.merchandise?.price?.amount || ""), //total price of the item.
-            item_brand: data?.merchandise?.product?.vendor || "", // brand of the item.(this is the example value)
-            ...(data?.merchandise?.product?.productType &&
-              data?.merchandise?.product?.productType != "" && {
-                item_category: data?.merchandise?.product?.productType || "",
-              }), //The category to which the product belongs to.
-            ...(data?.merchandise?.selectedOptions[0]?.value &&
-              data?.merchandise?.selectedOptions[0]?.value != "" &&
-              data?.merchandise?.selectedOptions.length != 1 && {
-                item_category2: data?.merchandise?.selectedOptions[0]?.value || "",
-              }), //size of the product.
-            ...(data?.merchandise?.selectedOptions[1]?.value &&
-              data?.merchandise?.selectedOptions[1]?.value != "" && {
-                item_variant: data?.merchandise?.selectedOptions[1]?.value || "",
-              }), // color of the product.
-            ...(data?.merchandise?.selectedOptions[0]?.value &&
-              data?.merchandise?.selectedOptions[0]?.value != "" &&
-              data?.merchandise?.selectedOptions.length == 1 && {
-                item_variant: data?.merchandise?.selectedOptions[0]?.value || "",
-              }), // color of the product.
-            item_category3: type,
-            //  item_list_name: "Category Page",//e.g. Filter results, Popular Picks For You ,Recently Viewed, Best sellers, Search Results, Personal Boutique etc.
-            //  item_list_id: "H3123", //ID of the list in which the item was presented to the user.
-            index: index + 1, // position of the item
-            quantity: data?.quantity, //quantity of the item
-
-            // promotion_id: "abc123",
-            // promotion_name: "shop now"
-          },
-        ],
-      },
-    });
+      // promotion_id: "abc123",
+      // promotion_name: "shop now"
+    };
+  });
+  gtmEvents({
+    event: "begin_checkout",
+    ecommerce: {
+      items: items,
+    },
   });
 };
 
@@ -109,19 +107,21 @@ export const addToCart = (data) => {
   gtmEvents({
     event: "add_to_cart",
     ecommerce: {
-      items: {
-        currency: data?.priceRange?.minVariantPrice?.currencyCode || "", // Currency
-        item_name: data?.title || "", // Name or ID is required.
-        item_id: data?.id ? data?.id.split("/")[data?.id.split("/").length - 1] : "", //ID of the item.
-        price: parseFloat(data?.priceRange?.minVariantPrice?.amount || ""), //total price of the item.
-        item_brand: data?.vendor || "", // brand of the item.(this is the example value)
-        ...(data?.productType && data?.productType != "" && { item_category: data?.productType || "" }), //The category to which the product belongs to.
-        ...(data?.size && data?.size != "" && { item_category2: data?.size }), //size of the product.
-        ...(data?.color && data?.color != "" && { item_variant: data?.color }),
-        index: data?.index,
-        ...(data.item_category3 && { item_category3: data.item_category3 }),
-        quantity: data?.quantity, //quantity of the item
-      },
+      items: [
+        {
+          currency: data?.priceRange?.minVariantPrice?.currencyCode || "", // Currency
+          item_name: data?.title || "", // Name or ID is required.
+          item_id: data?.id ? data?.id.split("/")[data?.id.split("/").length - 1] : "", //ID of the item.
+          price: parseFloat(data?.priceRange?.minVariantPrice?.amount || ""), //total price of the item.
+          item_brand: data?.vendor || "", // brand of the item.(this is the example value)
+          ...(data?.productType && data?.productType != "" && { item_category: data?.productType || "" }), //The category to which the product belongs to.
+          ...(data?.size && data?.size != "" && { item_category2: data?.size }), //size of the product.
+          ...(data?.color && data?.color != "" && { item_variant: data?.color }),
+          index: data?.index,
+          ...(data.item_category3 && { item_category3: data.item_category3 }),
+          quantity: data?.quantity, //quantity of the item
+        },
+      ],
     },
   });
 };
@@ -260,52 +260,51 @@ export const homeImpressiongmtEvent = (storeName) => {
 };
 
 export const homeProductsImpressiongmtEvent = (data: any) => {
-  data?.map((item: any, index) => {
-    return gtmEvents({
-      event: "view_item_list",
-      ecommerce: {
-        items: [
-          {
-            currency: item?.priceRange?.minVariantPrice?.currencyCode || "", // Currency
-            item_name: item?.title || "", // Name or ID is required.
-            item_id: item?.id ? item?.id.split("/")[item?.id.split("/").length - 1] : "", //ID of the item.
-            price: parseFloat(item?.priceRange?.minVariantPrice?.amount || ""), //total price of the item.
-            item_brand: item?.vendor || "", // brand of the item.(this is the example value)
-            ...(item?.productType && item?.productType != "" && { item_category: item?.productType || "" }), //The category to which the product belongs to.
-            index: index + 1,
+  let items = data?.map((item, index) => {
+    return {
+      currency: item?.priceRange?.minVariantPrice?.currencyCode || "", // Currency
+      item_name: item?.title || "", // Name or ID is required.
+      item_id: item?.id ? item?.id.split("/")[item?.id.split("/").length - 1] : "", //ID of the item.
+      price: parseFloat(item?.priceRange?.minVariantPrice?.amount || ""), //total price of the item.
+      item_brand: item?.vendor || "", // brand of the item.(this is the example value)
+      ...(item?.productType && item?.productType != "" && { item_category: item?.productType || "" }), //The category to which the product belongs to.
+      index: index + 1,
 
-            item_list_name: "new additions", //e.g. Filter results, Popular Picks For You ,Recently Viewed, Best sellers, Search Results, Personal Boutique etc.
+      item_list_name: "new additions", //e.g. Filter results, Popular Picks For You ,Recently Viewed, Best sellers, Search Results, Personal Boutique etc.
 
-            quantity: item?.totalInventory, //quantity of the item
-          },
-        ],
-      },
-    });
+      quantity: item?.totalInventory, //quantity of the item
+    };
+  });
+
+  gtmEvents({
+    event: "view_item_list",
+    ecommerce: {
+      items: items,
+    },
   });
 };
 
 export const productsImpressiongmtEvent = (data: any, type = "all products") => {
-  data.map((item: any, index) => {
-    return gtmEvents({
-      event: "view_item_list",
-      ecommerce: {
-        items: [
-          {
-            currency: item?.priceRange?.minVariantPrice?.currencyCode || "", // Currency
-            item_name: item?.title || "", // Name or ID is required.
-            item_id: item?.id ? item?.id.split("/")[item?.id.split("/").length - 1] : "", //ID of the item.
-            price: parseFloat(item?.priceRange?.minVariantPrice?.amount || ""), //total price of the item.
-            item_brand: item?.vendor || "", // brand of the item.(this is the example value)
-            ...(item?.productType && item?.productType != "" && { item_category: item?.productType || "" }), //The category to which the product belongs to.
-            index: index + 1,
+  let items = data.map((item, index) => {
+    return {
+      currency: item?.priceRange?.minVariantPrice?.currencyCode || "", // Currency
+      item_name: item?.title || "", // Name or ID is required.
+      item_id: item?.id ? item?.id.split("/")[item?.id.split("/").length - 1] : "", //ID of the item.
+      price: parseFloat(item?.priceRange?.minVariantPrice?.amount || ""), //total price of the item.
+      item_brand: item?.vendor || "", // brand of the item.(this is the example value)
+      ...(item?.productType && item?.productType != "" && { item_category: item?.productType || "" }), //The category to which the product belongs to.
+      index: index + 1,
 
-            item_list_name: type, //e.g. Filter results, Popular Picks For You ,Recently Viewed, Best sellers, Search Results, Personal Boutique etc.
+      item_list_name: type, //e.g. Filter results, Popular Picks For You ,Recently Viewed, Best sellers, Search Results, Personal Boutique etc.
 
-            quantity: item?.totalInventory, //quantity of the item
-          },
-        ],
-      },
-    });
+      quantity: item?.totalInventory, //quantity of the item
+    };
+  });
+  gtmEvents({
+    event: "view_item_list",
+    ecommerce: {
+      items: items,
+    },
   });
 };
 
@@ -336,36 +335,38 @@ export const increaseCartProduct = (data) => {
   gtmEvents({
     event: "add_to_cart",
     ecommerce: {
-      items: {
-        currency: data?.merchandise?.price?.currencyCode || "", // Currency
-        item_name: data?.merchandise?.product?.title || "", // Name or ID is required.
-        item_id: data?.merchandise?.product?.id
-          ? data?.merchandise?.product?.id.split("/")[data?.merchandise?.product?.id.split("/").length - 1]
-          : "", //ID of the item.
-        price: parseFloat(data?.merchandise?.price?.amount || ""), //total price of the item.
-        item_brand: data?.merchandise?.product?.vendor || "", // brand of the item.(this is the example value)
-        ...(data?.merchandise?.product?.productType &&
-          data?.merchandise?.product?.productType != "" && {
-            item_category: data?.merchandise?.product?.productType || "",
-          }), //The category to which the product belongs to.
-        ...(data?.merchandise?.selectedOptions[0]?.value &&
-          data?.merchandise?.selectedOptions[0]?.value != "" &&
-          data?.merchandise?.selectedOptions.length != 1 && {
-            item_category2: data?.merchandise?.selectedOptions[0]?.value || "",
-          }), //size of the product.
-        ...(data?.merchandise?.selectedOptions[1]?.value &&
-          data?.merchandise?.selectedOptions[1]?.value != "" && {
-            item_variant: data?.merchandise?.selectedOptions[1]?.value || "",
-          }), // color of the product.
-        ...(data?.merchandise?.selectedOptions[0]?.value &&
-          data?.merchandise?.selectedOptions[0]?.value != "" &&
-          data?.merchandise?.selectedOptions.length == 1 && {
-            item_variant: data?.merchandise?.selectedOptions[0]?.value || "",
-          }),
-        index: data?.index ? data?.index + 1 : 1,
+      items: [
+        {
+          currency: data?.merchandise?.price?.currencyCode || "", // Currency
+          item_name: data?.merchandise?.product?.title || "", // Name or ID is required.
+          item_id: data?.merchandise?.product?.id
+            ? data?.merchandise?.product?.id.split("/")[data?.merchandise?.product?.id.split("/").length - 1]
+            : "", //ID of the item.
+          price: parseFloat(data?.merchandise?.price?.amount || ""), //total price of the item.
+          item_brand: data?.merchandise?.product?.vendor || "", // brand of the item.(this is the example value)
+          ...(data?.merchandise?.product?.productType &&
+            data?.merchandise?.product?.productType != "" && {
+              item_category: data?.merchandise?.product?.productType || "",
+            }), //The category to which the product belongs to.
+          ...(data?.merchandise?.selectedOptions[0]?.value &&
+            data?.merchandise?.selectedOptions[0]?.value != "" &&
+            data?.merchandise?.selectedOptions.length != 1 && {
+              item_category2: data?.merchandise?.selectedOptions[0]?.value || "",
+            }), //size of the product.
+          ...(data?.merchandise?.selectedOptions[1]?.value &&
+            data?.merchandise?.selectedOptions[1]?.value != "" && {
+              item_variant: data?.merchandise?.selectedOptions[1]?.value || "",
+            }), // color of the product.
+          ...(data?.merchandise?.selectedOptions[0]?.value &&
+            data?.merchandise?.selectedOptions[0]?.value != "" &&
+            data?.merchandise?.selectedOptions.length == 1 && {
+              item_variant: data?.merchandise?.selectedOptions[0]?.value || "",
+            }),
+          index: data?.index ? data?.index + 1 : 1,
 
-        quantity: data?.quantity, //quantity of the item
-      },
+          quantity: data?.quantity, //quantity of the item
+        },
+      ],
     },
   });
 };

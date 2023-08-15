c5c2c85a504a05431ae320954bfa169e657196b7
Brandon Chilchik <brandon.chilchik@inship.com.au>
2023-08-15 09:12:52 +0300

    debug

--- api/restApi/client.ts
+++ api/restApi/client.ts
@@ -3,9 +3,6 @@ import axios, { AxiosInstance } from "axios";
 const apiClient: AxiosInstance = axios.create({
   responseType: "json",
   baseURL: process.env.NEXT_PUBLIC_API_DEV_BASE_URL,
-  headers: {
-    "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_SHOPIFY_TOKEN,
-  },
 });
 
 export default apiClient;

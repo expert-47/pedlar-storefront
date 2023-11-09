8addf5f15fc04431d09315e7b2d2fe82290fcd18
Ahtisham Shahzad <ahtisham@gsoftconsulting.com>
2023-11-09 10:35:57 +0500

    feat: refector code

--- components/product/components/options.tsx
+++ components/product/components/options.tsx
@@ -19,31 +19,33 @@ function Options(props: any) {
 
   return (
     <Grid container item xs={12} sm={12} md={12} lg={12} gap={22} sx={styles.container}>
-      {newAdditionData?.options.map((item, index) => (
-        <Grid item xs={12} sm={10} md={6} lg={6}>
-          <Typography sx={styles.typography}>{item.name}</Typography>
-          <FormControl sx={{ width: "100%" }}>
-            <Select
-              value={priceFilter[index].value}
-              onChange={(event) => {
-                handleChange(event.target.value, item.name, index);
-              }}
-              displayEmpty
-              sx={styles.select}
-              IconComponent={item?.values?.length === 1 ? "" : KeyboardArrowDownIcon}
-              disabled={item?.values?.length == 1}
-            >
-              {item?.values?.map((val: any, index: any) => {
-                return (
-                  <MenuItem key={index} value={val}>
-                    {val}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-        </Grid>
-      ))}
+      {newAdditionData?.options[0]?.name == "Title" && newAdditionData?.options[0]?.values[0] == "Default Title"
+        ? null
+        : newAdditionData?.options?.map((item, index) => (
+            <Grid item xs={12} sm={10} md={6} lg={6}>
+              <Typography sx={styles.typography}>{item.name}</Typography>
+              <FormControl sx={{ width: "100%" }}>
+                <Select
+                  value={priceFilter[index].value}
+                  onChange={(event) => {
+                    handleChange(event.target.value, item.name, index);
+                  }}
+                  displayEmpty
+                  sx={styles.select}
+                  IconComponent={item?.values?.length === 1 ? "" : KeyboardArrowDownIcon}
+                  disabled={item?.values?.length == 1}
+                >
+                  {item?.values?.map((val: any, index: any) => {
+                    return (
+                      <MenuItem key={index} value={val}>
+                        {val}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            </Grid>
+          ))}
     </Grid>
   );
 }
